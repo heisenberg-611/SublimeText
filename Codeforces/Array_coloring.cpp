5576880f8127cb6bc0da293d407757826ec9c442
dhrubojyoti <lnsaha1214@gmail.com>
2024-05-09 12:30:55 +0600

    09/04/24

---
+++ Codeforces/Array_coloring.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<climits>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+#include<string>
+#include<assert.h>
+#include<utility>
+#include<bits/stdc++.h>
+using namespace std;
+#define len(x) int((x).size())
+#define ll long long
+#define ld long double
+#define pi pair<int,int>
+#define nl '\n';
+#define vi vector<int>
+void solve() {
+	int n;
+    cin>>n;
+    
+    vi arr(n);
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+    
+    int odd_count = 0;
+    
+    for(int i=0; i<n; i++){
+    	if(arr[i]%2 == 1){
+    		odd_count++;
+    	}
+    }
+    
+    if(odd_count % 2 == 0){
+    	cout<<"Yes"<<nl;
+    }
+    else{
+    	cout<<"No"<<nl;
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
