dafc319bc1079b132d5dde83d461d0f32ec8cfdf
dhrubojyoti <lnsaha1214@gmail.com>
2024-06-19 22:11:20 +0600

    19/06/24

--- CSE111/Lab 2/Task9.java
+++ CSE111/Lab 2/Task9.java
@@ -13,29 +13,22 @@
 // 	}
 // }
 
-import java.util.Scanner;
-import java.util.ArrayList;
-public class Task9 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int c = n;
-		int digit_count = 0;
-		ArrayList<Integer> f = new ArrayList<>();
+// import java.util.Scanner;
 
-		while (n != 0) {
-			digit_count++;
-			n = n / 10;
-		}
-		for (int i = 1; i <= digit_count; i++) {
-			int d = c % 10;
-			int y = d*7;
-			f.add(y);
-			c = c / 10;
-		}
-		for(int i = f.size()-1; i>=0; i--){
-			System.out.print(f.get(i) + " ");
-		}
-		sc.close();
-	}
-}
+// public class Task9 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         String ans = "";
+
+//         while (n != 0) {
+//             int d = n % 10;
+//             int y = d * 7;
+//             ans = y + " " + ans;
+//             n = n / 10;
+//         }
+
+//         System.out.println(ans);
+//         sc.close();
+//     }
+// }
