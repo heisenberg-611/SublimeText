7e1a30ccc241687a60be6b7dcb75e1c2b499539e
heisenberg-611 <lnsaha1214@gmail.com>
2025-07-14 23:49:32 +0600

    ...

--- CppOOP/hello.java
+++ CppOOP/hello.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class hello{
 	public static void main(String[] args){
-		Scanner sc = new Scanner();
+		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		System.out.println(n);
 	}
